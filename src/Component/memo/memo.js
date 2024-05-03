13791457b913f5481e690aba437ff80676c7c5e5
djetrl <leshadjet@Mail.ru>
2024-05-03 22:54:25 +0700

    add russian text

--- src/Component/memo/memo.js
+++ src/Component/memo/memo.js
@@ -9,19 +9,19 @@ const Memo = ()=>{
         <section className="memo">
         <div className="container">
             <h2 className="memo__title">“Я нашёл животное, нуждающееся в помощи”</h2>
-            <p className="memo__text">В своём стремлении улучшить пользовательский опыт мы упускаем, что акционеры крупнейших компаний подвергнуты целой серии независимых исследований. Учитывая ключевые сценарии поведения, понимание сути ресурсосберегающих технологий напрямую зависит от поэтапного и последовательного развития общества.</p>
+            <p className="memo__text">Если вы нашли питомца, позвонили в приют и у вас его забрали — это не означает, что вы его спасли. Это животное будут спасать сотрудники, волонтёры и неравнодушные подписчики приюта:</p>
             <div className="memo__block">
                 <div className="memo__block__item">
                     <img src={attention} alt="attention" className="memo__block__img" />
-                    <p className="memo__block__text">Равным образом, реализация намеченных плановых заданий </p>
+                    <p className="memo__block__text">Не перекладывайте ответственность на других людей.</p>
                 </div>
                 <div className="memo__block__item">
                     <img src={home} alt="home" className="memo__block__img" />
-                    <p className="memo__block__text">Равным образом, реализация намеченных плановых заданий </p>
+                    <p className="memo__block__text">собирать деньги, лечить и искать ему дом.</p>
                 </div>
                 <div className="memo__block__item">
                     <img src={phone} alt="phone" className="memo__block__img" />
-                    <p className="memo__block__text">Равным образом, реализация намеченных плановых заданий </p>
+                    <p className="memo__block__text"> Постарайтесь и в дальнейшем участвовать в судьбе собаки или кошки, помогать приюту и другим животным!</p>
                 </div>
             </div>
         </div>
